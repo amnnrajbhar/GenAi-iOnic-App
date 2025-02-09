c456c302e92154e52261ef9ec9a637023ac8ba46
Aman Rajbhar <amanrajbhar289@gmail.com>
2025-02-09 15:15:47 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ai-text-app',
+  webDir: 'www'
+};
+
+export default config;
